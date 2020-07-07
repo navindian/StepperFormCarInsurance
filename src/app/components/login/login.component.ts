a6ef3e090e4b11ebb6e536a185c4a14745583aa2
KARAN <karan@KARANs-MacBook-Air.local>
2020-07-07 19:16:00 +0530

    login component change into material

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,6 +1,7 @@
 import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { Router } from '@angular/router';
 import { LoginService } from './login.service';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -8,7 +9,7 @@ import { LoginService } from './login.service';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
-  constructor(private router: Router, private loginService: LoginService) {}
+  constructor(private router: Router, private loginService: LoginService,private fb:FormBuilder) {}
 
   @Output() onLoginError = new EventEmitter<any>();
   @Output() onLoginSuccess = new EventEmitter<any>();
@@ -17,15 +18,25 @@ export class LoginComponent implements OnInit {
   email: string;
   password: string;
   errorMessage;
+  loginForm:FormGroup
 
-  ngOnInit() {}
+  ngOnInit() {
+this.loginForm=this.fb.group({
+  email:['',[Validators.required]],
+  password:['',[Validators.required]]
+})
+
+
+  }
   login(): void {
+
+   
+    
     this.errorMessage=null;
     this.loginService
-      .getLoginData({
-        email: this.email,
-        password: this.password
-      })
+      .getLoginData(
+        this.loginForm.value
+      )
       .subscribe(
         res => {
           const response = JSON.parse(JSON.stringify(res));
